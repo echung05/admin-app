4d27097022cb95a4f74f0ea43f4d2945cca3ce21
Esther Chung <e.chung059@gmail.com>
2020-05-27 17:30:57 -0400

    Created Add/Delete Student Functionality, Connected to Firebase for Login/Database

--- src/App.js
+++ src/App.js
@@ -1,26 +1,173 @@
-import React from 'react';
-import logo from './logo.svg';
+import withFirebaseAuth from 'react-with-firebase-auth'
+import Firebase from "firebase";
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+import firebaseConfig from './firebaseConfig';
+import Navbar from "react-bootstrap/Navbar"
+import Button from "react-bootstrap/Button"
+import Card from "react-bootstrap/Card"
+import Container from "react-bootstrap/Container"
+import Form from "react-bootstrap/Form"
+import Accordion from "react-bootstrap/Accordion"
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+class App extends Component {
+  constructor(props) {
+    // Firebase.initializeApp(firebaseConfig);
+    super(props);
+    this.state = ({
+      currStudent: '',
+      stuTeacher: '',
+      stuClass: '',
+      stuDesc: '',
+      students: []
+    });
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const studentsRef = firebase.database().ref('students');
+    const student = {
+      student: this.state.currStudent,
+      teacher: this.state.stuTeacher,
+      class: this.state.stuClass,
+      desc: this.state.stuDesc
+    }
+    studentsRef.push(student);
+    this.setState({
+      currStudent: '',
+      stuTeacher: '',
+      stuClass: '',
+      stuDesc: ''
+    });
+  }
+
+  componentDidMount() {
+    const studentsRef = firebase.database().ref('students');
+    studentsRef.on('value', (snapshot) => {
+      let students = snapshot.val();
+      let newState = [];
+      for (let stu in students) {
+        newState.push({
+          id: stu,
+          student: students[stu].student,
+          teacher: students[stu].teacher,
+          class: students[stu].class,
+          desc: students[stu].desc
+        });
+      }
+      this.setState({
+        students: newState
+      });
+    });
+  }
+  removeStudent(studentId) {
+    const stuRef = firebase.database().ref(`/students/${studentId}`);
+    stuRef.remove();
+  }
+
+  render() {
+    const {
+      user,
+      signOut,
+      signInWithGoogle,
+    } = this.props;
+
+    return (
+      <div className="App">
+        <Navbar bg="light" expand="lg">
+          <Navbar.Brand href="#home">
+            <img
+              src="https://static.thenounproject.com/png/1853615-200.png"
+              width="40"
+              height="40"
+              className="d-inline-block align-top"
+              alt="icon"
+            />
+          </Navbar.Brand>
+          <Navbar.Brand href="#home">Thomas Jefferson Elementary School</Navbar.Brand>
+        </Navbar>
+        <Container>
+          <Card bg="light" style={{ margin: "0 auto", width: "20vw", marginTop: "2vh", float: "none" }}>{
+            user
+              ? <Card.Header>Hello, {user.displayName}</Card.Header>
+              : <Card.Header>Please sign in.</Card.Header>
+          }
+            {
+              user
+                ? <Card.Body><Button variant="info" onClick={signOut}>Sign out</Button></Card.Body>
+                : <Card.Body><Button variant="info" onClick={signInWithGoogle}>Sign in with Google</Button></Card.Body>
+            }</Card>
+        </Container>
+        <Card style={{ margin: "0 auto", marginTop: "5vh", width: "50vw", float: "none" }}>
+          <Container>
+            <section className='add-item'>
+              <form onSubmit={this.handleSubmit}>
+                <input type="text" name="currStudent" placeholder="Full Name" onChange={this.handleChange} value={this.state.currStudent} />
+                <input type="text" name="stuTeacher" placeholder="Teacher" onChange={this.handleChange} value={this.state.stuTeacher} />
+                <input type="text" name="stuClass" placeholder="Class" onChange={this.handleChange} value={this.state.stuClass} />
+                <input type="text" name="stuDesc" placeholder="Basic Info" onChange={this.handleChange} value={this.state.stuDesc} />
+                <button>Add Student</button>
+              </form>
+            </section>
+          </Container>
+        </Card>
+
+        <Accordion style={{ margin: "0 auto", marginTop: "5vh", marginBottom: "10vh", width: "30vw", float: "none" }}>
+          {this.state.students.map((s) => {
+            return (
+              <Card key={s.id}>
+                <Card.Header text="dark">
+                  <Accordion.Toggle as={Button} variant="light" eventKey="0">
+                    Name: {s.student}
+                  </Accordion.Toggle>
+                </Card.Header>
+                <Accordion.Collapse eventKey="0">
+                  <Card.Body text="dark">
+                    <div>Teacher: {s.teacher}</div>
+                    <div>Class: {s.class}</div>
+                    <div>Information: {s.desc}</div>
+                    <Button
+                      variant="outline-info"
+                    >
+                      Edit
+                </Button> {' '}
+                    <Button
+                      variant="outline-danger"
+                      onClick={() => this.removeStudent(s.id)}>
+                      Delete
+                </Button>
+                  </Card.Body>
+                </Accordion.Collapse>
+              </Card>
+            );
+          })}
+        </Accordion>
+
+      </div>
+    );
+  }
 }
 
-export default App;
+const firebaseAppAuth = firebaseApp.auth();
+
+const providers = {
+  googleProvider: new firebase.auth.GoogleAuthProvider(),
+};
+
+export default withFirebaseAuth({
+  providers,
+  firebaseAppAuth,
+})(App);
---
+++ src/firebaseConfig.js
@@ -0,0 +1,12 @@
+const config = {
+    apiKey: "AIzaSyCuElgj2aA-43aVraBgoPAgYzwDIn1Y36c",
+    authDomain: "tjes-admin.firebaseapp.com",
+    databaseURL: "https://tjes-admin.firebaseio.com",
+    projectId: "tjes-admin",
+    storageBucket: "tjes-admin.appspot.com",
+    messagingSenderId: "416840521146",
+    appId: "1:416840521146:web:61784c194d63057fb27983",
+    measurementId: "G-0CBZPBXEVR"
+};
+
+export default config;
